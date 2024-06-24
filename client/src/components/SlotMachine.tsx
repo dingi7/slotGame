b03ed8b3889ae42c6eb015dba9443ba4c93b15fb
Mityo Draganov <mityodraganow@gmail.com>
2024-06-24 22:41:32 +0300

    Update SlotMachine.tsx

--- client/src/components/SlotMachine.tsx
+++ client/src/components/SlotMachine.tsx
@@ -4,38 +4,41 @@ import React, { useEffect, useState } from "react";
 // Define asset URLs
 
 export const SlotMachine = () => {
-    const [assets, setAssets] = useState([
-        "https://pixijs.com/assets/eggHead.png",
-        "https://pixijs.com/assets/flowerTop.png",
-        "https://pixijs.com/assets/helmlok.png",
-        "https://pixijs.com/assets/skully.png",
-    ]);
+  const [assets, setAssets] = useState([
+    "https://pixijs.com/assets/eggHead.png",
+    "https://pixijs.com/assets/flowerTop.png",
+    "https://pixijs.com/assets/helmlok.png",
+    "https://pixijs.com/assets/skully.png",
+  ]);
 
-    const columns = 3;
-    const [assetsMatrix, setAssetsMatrix] = useState<string[][]>(
-        Array.from({ length: columns }, () => [])
-    );
+  const columns = 3;
+  const [assetsMatrix, setAssetsMatrix] = useState<string[][]>(
+    Array.from({ length: columns }, () => [])
+  );
 
-    useEffect(() => {
-        // Initialize a new matrix
-        const matrix: string[][] = Array.from({ length: columns }, () => []);
+  useEffect(() => {
+    // Initialize a new matrix
+    const matrix: string[][] = Array.from({ length: columns }, () => []);
 
-        // Duplicate assets across columns
-        assets.forEach((asset) => {
-            for (let i = 0; i < columns; i++) {
-                matrix[i].push(asset);
-            }
-        });
-        setAssetsMatrix(matrix);
-    }, [assets, columns]);
+    // Duplicate assets across columns
+    assets.forEach((asset) => {
+      for (let i = 0; i < columns; i++) {
+        matrix[i].push(asset);
+      }
+    });
+    setAssetsMatrix(matrix);
+  }, [assets, columns]);
 
-    const shuffleMatrix = () => {
+  const shuffleMatrix = () => {
     setAssetsMatrix((prevMatrix) => {
       const newMatrix = prevMatrix.map((column) => {
         const shuffledColumn = [...column];
         for (let i = shuffledColumn.length - 1; i > 0; i--) {
           const j = Math.floor(Math.random() * (i + 1));
-          [shuffledColumn[i], shuffledColumn[j]] = [shuffledColumn[j], shuffledColumn[i]];
+          [shuffledColumn[i], shuffledColumn[j]] = [
+            shuffledColumn[j],
+            shuffledColumn[i],
+          ];
         }
         return shuffledColumn;
       });
@@ -43,63 +46,69 @@ export const SlotMachine = () => {
     });
   };
 
-    const windowWidth = window.innerWidth;
-  
-    return (
-        <div className="w-full h-full flex flex-col justify-center items-center gap-5">
-            <div>
-                <p className="flex gap-[5%]">
-                    <span className="w-fit">Mega Jackpot</span>{" "}
-                    <span>77777</span>
-                </p>
-            </div>
-            <div>
-                <Stage
-                    options={{ background: 0xfffff }}
-                    width={windowWidth * 0.4}
-                    height={windowWidth * 0.3}
-                >
-                    {assetsMatrix.map((column, index) => (
-                        <Container x={index * windowWidth * 0.15}>
-                            {column.map((asset, index) => (
-                                <Sprite
-                                    key={index}
-                                    image={asset}
-                                    y={index * windowWidth * 0.1}
-                                    width={windowWidth * 0.1}
-                                    height={windowWidth * 0.1}
-                                  
-                />
-                            ))}
-                        </Container>
-                    ))}
-                </Stage>
-            </div>
+  const windowWidth = window.innerWidth;
 
-            <div className="w-full flex gap-[10%] justify-center items-center">
-                <div className="flex gap-4 text-2xl">
-                    <button className="border-slate-200 border-2 py-[8%] px-4 rounded-tl-lg rounded-bl-lg">
-  -
-                    </button>
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center gap-5">
+      <div>
+        <p className="flex gap-[5%]">
+          <span className="w-fit">Mega Jackpot</span> <span>77777</span>
+        </p>
+      </div>
+      <div>
+        <Stage
+          options={{ background: 0xfffff }}
+          width={windowWidth * 0.4}
+          height={windowWidth * 0.3}
+        >
+          {assetsMatrix.map((column, index) => (
+            <Container x={index * windowWidth * 0.15}>
+              {column.map((asset, index) => (
+                <Sprite
+                  key={index}
+                  image={asset}
+                  y={index * windowWidth * 0.1}
+                  width={windowWidth * 0.1}
+                  height={windowWidth * 0.1}
+                />
+              ))}
+            </Container>
+          ))}
+        </Stage>
+      </div>
+
+      <div className="w-full flex gap-[10%] justify-center items-center">
+        <div className="flex gap-4 text-2xl border-2 rounded-lg p-[0.2%]">
+          <button className="border-slate-200 border-2 py-[8%] px-4 rounded-tl-lg rounded-bl-lg shadow">
+            -
+          </button>
           <div className="flex flex-col justify-center">
-                        <p className="text-2xl">500</p>
-                        <p className="text-xl uppercase text-center">total bet</p>
-                    </div>
-          <button className="border-slate-200 border-2 py-[8%] px-4 rounded-tr-lg rounded-br-lg">
-  +
-                    </button>
+            <p className="text-2xl">500</p>
+            <p className="text-xl uppercase text-center">total bet</p>
+          </div>
+          <button className="border-slate-200 border-2 py-[8%] px-4 rounded-tr-lg rounded-br-lg shadow">
+            +
+          </button>
         </div>
 
         <div className="uppercase flex flex-col gap-4">
           <p className="font-bold text-2xl">0</p>
           <p>win</p>
-                </div>
+        </div>
 
-                <button onClick={shuffleMatrix} className="uppercase">
+        <button className="uppercase ">
+            <span></span>
+            <p className="flex flex-col">
+                <span>max </span>
+                <span>bet</span>
+            </p>
+        </button>
+
+        <button onClick={shuffleMatrix} className="uppercase">
           <p>Spin</p>
           <p>hold for auto</p>
         </button>
-            </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
