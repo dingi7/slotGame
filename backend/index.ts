7c5547efd16279584ef6b22caa0a6b3da1e48f2e
Ivelin0 <ivelin.ivanov24t@gmail.com>
2024-06-24 20:32:31 +0300

    prettier format

--- backend/index.ts
+++ backend/index.ts
@@ -1,20 +1,20 @@
-import { play } from "./slotMachine/slotLogic";
+import { play } from "./operations/slotMachine/slotLogic.operation";
 
 function simulate(plays: number, betAmount: number): number {
-    let totalBet = 0;
-    let totalPayout = 0;
+  let totalBet = 0;
+  let totalPayout = 0;
 
-    for (let i = 0; i < plays; i++) {
-        totalBet += betAmount;
-        const [payout, result] = play(betAmount);
-        totalPayout += Number(payout);
-    }
+  for (let i = 0; i < plays; i++) {
+    totalBet += betAmount;
+    const [payout, result] = play(betAmount);
+    totalPayout += Number(payout);
+  }
 
-    const RTP = (totalPayout / totalBet) * 100;
-    console.log(`Total Bet: ${totalBet} - Total Payout: ${totalPayout}`);
-    console.log(Number(totalBet) - Number(totalPayout));
-    
-    return RTP;
+  const RTP = (totalPayout / totalBet) * 100;
+  console.log(`Total Bet: ${totalBet} - Total Payout: ${totalPayout}`);
+  console.log(Number(totalBet) - Number(totalPayout));
+
+  return RTP;
 }
 
 // Simulate 500,000 plays with a bet amount of 10 units
@@ -22,5 +22,7 @@ const plays = 100000;
 const betAmount = 10;
 const RTP = simulate(plays, betAmount);
 
-console.log(`Simulated ${plays} plays with a bet of ${betAmount} units per spin.`);
+console.log(
+  `Simulated ${plays} plays with a bet of ${betAmount} units per spin.`
+);
 console.log(`Return to Player (RTP): ${RTP.toFixed(2)}%`);
