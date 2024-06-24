ab3a20245ec299cf722b5b09bf7f82b8c00d8c23
Mityo Draganov <mityodraganow@gmail.com>
2024-06-24 21:54:41 +0300

    fix tailwind

--- client/src/components/SlotMachine.tsx
+++ client/src/components/SlotMachine.tsx
@@ -16,35 +16,66 @@ export const SlotMachine = () => {
     Array.from({ length: columns }, () => [])
   );
 
-useEffect(() => {
-  // Initialize a new matrix
-  const matrix: string[][] = Array.from({ length: columns }, () => []);
+  useEffect(() => {
+    // Initialize a new matrix
+    const matrix: string[][] = Array.from({ length: columns }, () => []);
 
-  // Duplicate assets across columns
-  assets.forEach((asset) => {
-    for (let i = 0; i < columns; i++) {
-      matrix[i].push(asset);
-    }
-  });
+    // Duplicate assets across columns
+    assets.forEach((asset) => {
+      for (let i = 0; i < columns; i++) {
+        matrix[i].push(asset);
+      }
+    });
 
-  // Set the state with the newly created matrix
-  setAssetsMatrix(matrix);
-}, [assets, columns]);
+    // Set the state with the newly created matrix
+    setAssetsMatrix(matrix);
+  }, [assets, columns]);
 
-
-
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
   return (
-    <div>
-      <Stage options={{ background: 0xffffff }}>
-        {assetsMatrix.map((column, index) => (
-          <Container x={index * 120}>
-            {column.map((asset, index) => (
-              <Sprite key={index} image={asset} y={index * 120} />
-            ))}
-          </Container>
-        ))}
-      </Stage>
-      <button>Click</button>
+    <div className="w-full h-full flex flex-col justify-center items-center gap-5">
+      <div>
+        <p className="flex gap-[5%]">
+          <span className="w-fit">Mega Jackpot</span> <span>77777</span>
+        </p>
+      </div>
+      <div>
+        <Stage
+          options={{ background: 0xfffff }}
+          width={windowWidth * 0.4}
+          height={windowWidth * 0.3}
+        >
+          {assetsMatrix.map((column, index) => (
+            <Container x={index * windowWidth * 0.15}>
+              {column.map((asset, index) => (
+                <Sprite
+                  key={index}
+                  image={asset}
+                  y={index * windowWidth * 0.1}
+                  width={windowWidth * 0.1}
+                  height={windowWidth * 0.1}
+                />
+              ))}
+            </Container>
+          ))}
+        </Stage>
+      </div>
+
+      <div className="flex gap-4">
+        <div className="flex gap-4 text-2xl">
+          <button>-</button>
+          
+          <p className="flex flex-col">
+            <span>500</span>
+            <span>total bet</span>
+          </p>
+          
+          <button>+</button>
+        </div>
+
+        <button>Spin</button>
+      </div>
     </div>
   );
 };
