0e52d5feac90a96acc691323830abd4f3f16ec21
Stefan Todorov <79049907+StefanTodorov32@users.noreply.github.com>
2024-06-25 15:39:02 +0300

    draw a line when winning

--- client/src/components/SlotMachine.tsx
+++ client/src/components/SlotMachine.tsx
@@ -1,25 +1,27 @@
 import * as PIXI from "pixi.js";
-
 import { Container, Graphics, Sprite, Stage } from "@pixi/react";
 import { useEffect, useRef, useState } from "react";
-
 import { RefreshCcw } from "lucide-react";
 import { sendSpinRequest } from "../api/requests";
 import slotBackground from "../assets/slot-background.jpg";
 
-// Define asset URLs
 const assets = [
-  "https://pixijs.com/assets/eggHead.png",
-  "https://pixijs.com/assets/flowerTop.png",
-  "https://pixijs.com/assets/helmlok.png",
-  "https://pixijs.com/assets/skully.png",
+  { image: "https://png.pngtree.com/png-clipart/20210123/ourmid/pngtree-black-colorful-number-1-png-image_2786493.jpg", value: 1 },
+  { image: "https://png.pngtree.com/png-clipart/20210123/ourmid/pngtree-black-colorful-number-1-png-image_2786493.jpg", value: 2 },
+  { image: "https://png.pngtree.com/png-clipart/20210123/ourmid/pngtree-black-colorful-number-1-png-image_2786493.jpg", value: 3 },
+  { image: "https://png.pngtree.com/png-clipart/20210123/ourmid/pngtree-black-colorful-number-1-png-image_2786493.jpg", value: 4 },
+  { image: "https://png.pngtree.com/png-clipart/20210123/ourmid/pngtree-black-colorful-number-1-png-image_2786493.jpg", value: 5 },
+  { image: "https://png.pngtree.com/png-clipart/20210123/ourmid/pngtree-black-colorful-number-1-png-image_2786493.jpg", value: 6 },
+  { image: "https://png.pngtree.com/png-clipart/20210123/ourmid/pngtree-black-colorful-number-1-png-image_2786493.jpg", value: 7 },
+  { image: "https://png.pngtree.com/png-clipart/20210123/ourmid/pngtree-black-colorful-number-1-png-image_2786493.jpg", value: 8 },
+  { image: "https://png.pngtree.com/png-clipart/20210123/ourmid/pngtree-black-colorful-number-1-png-image_2786493.jpg", value: 9 },
 ];
 const columns = 3;
 const rows = 3;
-const stopTime = 3000; // Time in ms to stop each column
-const totalSpinTime = 4000; // Total spin time in ms
+const stopTime = 1000; // Time in ms to stop each column
+const totalSpinTime = 3000; // Total spin time in ms
 
-type Matrix = string[][];
+type Matrix = { image: string, value: number }[][];
 
 const initializeAssetsMatrix = (): Matrix => {
   return Array.from({ length: columns }, () =>
@@ -49,12 +51,13 @@ const lerp = (start: number, end: number, t: number): number => {
   return start + (end - start) * t;
 };
 
+// Modified easing function for slower end
 const backout = (t: number): number => {
   const s = 1.70158;
   return --t * t * ((s + 1) * t + s) + 1;
 };
 
-const getRandomAsset = (): string => {
+const getRandomAsset = (): { image: string, value: number } => {
   return assets[Math.floor(Math.random() * assets.length)];
 };
 
@@ -65,27 +68,41 @@ const updateAssetsMatrix = (matrix: Matrix): Matrix => {
   ]);
 };
 
+const setWinningLine = (matrix: Matrix, value: number): Matrix => {
+  const winningLine = assets.find(asset => asset.value === value);
+  if (!winningLine) return matrix;
+
+  return matrix.map(column => {
+    const newColumn = [...column];
+    newColumn[1] = winningLine; // Set the middle row to the winning value
+    return newColumn;
+  });
+};
+
 export const SlotMachine: React.FC = () => {
-  //const betInterval = 10;
   const fixedBetAmounts = [20, 40, 100, 200];
+  const [win, setWin] = useState<boolean>(false);
+  const [payout, setPayout] = useState<number>(0);
   const [betAmount, setBetAmount] = useState<number>(fixedBetAmounts[0]);
   const [assetsMatrix, setAssetsMatrix] = useState<Matrix>(
     initializeAssetsMatrix
   );
   const [spinning, setSpinning] = useState<boolean>(false);
   const [positions, setPositions] = useState<number[]>(Array(columns).fill(0));
+  const [showLine, setShowLine] = useState<boolean>(false);
+
   const ticker = useRef(new PIXI.Ticker());
   const startTimes = useRef<number[]>(Array(columns).fill(0));
   const stopTimes = useRef<number[]>(Array(columns).fill(0));
   const speeds = useRef<number[]>(Array(columns).fill(0));
   const windowWidth = window.innerWidth;
-  const slotHeight = windowWidth * 0.1 * (windowWidth <= 768 ? 2 : 1);
+  const slotHeight = windowWidth * 0.1;
   const totalHeight = slotHeight * rows;
   const intervalId = useRef<number | null>(null);
+  const lineIntervalId = useRef<number | null>(null);
 
   useEffect(() => {
-    const handleTick = (ticker: PIXI.Ticker) => {
-      const delta = ticker.deltaTime;
+    const handleTick = () => {
       if (spinning) {
         const now = Date.now();
         setPositions((prevPositions) =>
@@ -94,7 +111,7 @@ export const SlotMachine: React.FC = () => {
               speeds.current[index] = 0;
               return snapToGrid(pos, slotHeight);
             }
-
+  
             const elapsed = now - startTimes.current[index];
             const duration =
               stopTimes.current[index] - startTimes.current[index];
@@ -102,18 +119,32 @@ export const SlotMachine: React.FC = () => {
             const easedT = backout(t);
             const newPos = lerp(
               0,
-              speeds.current[index] * (totalSpinTime / 2),
+              speeds.current[index] * (totalSpinTime / 4), // Adjusted speed multiplier
               easedT
-            ); // Adjusted speed multiplier
-            return pos + newPos * delta;
+            );
+            return (pos + newPos) % (totalHeight * 2);
           })
         );
-
+  
         if (speeds.current.every((speed) => speed === 0)) {
           setSpinning(false);
           setAssetsMatrix((prevMatrix) =>
             realignMatrix(prevMatrix, positions, slotHeight)
           );
+          if (win) {
+            setAssetsMatrix(prevMatrix => setWinningLine(prevMatrix, payout));
+            setShowLine(true); // Start by showing the line
+            lineIntervalId.current = window.setInterval(() => {
+              setShowLine(prevShowLine => !prevShowLine); // Toggle the line visibility
+            }, 500); // Flash every 500ms
+            setTimeout(() => {
+              if (lineIntervalId.current !== null) {
+                clearInterval(lineIntervalId.current);
+                lineIntervalId.current = null;
+              }
+              setShowLine(false); // Ensure the line is hidden after 4 seconds
+            }, 4000);
+          }
           if (intervalId.current !== null) {
             clearInterval(intervalId.current);
             intervalId.current = null;
@@ -121,28 +152,30 @@ export const SlotMachine: React.FC = () => {
         }
       }
     };
-
+  
     ticker.current.add(handleTick);
     ticker.current.start();
-
+  
     return () => {
       ticker.current.remove(handleTick);
       ticker.current.stop();
       if (intervalId.current !== null) {
         clearInterval(intervalId.current);
       }
+      if (lineIntervalId.current !== null) {
+        clearInterval(lineIntervalId.current);
+      }
     };
-  }, [spinning, slotHeight]);
+  }, [spinning, slotHeight, win, payout]);
+  
+  
 
   const startSpinning = async () => {
     setAssetsMatrix(initializeAssetsMatrix()); // Reset the matrix when spinning starts
     setSpinning(true);
-    const result = await sendSpinRequest(betAmount);
-    console.log(result);
-
     speeds.current = Array(columns)
       .fill(0)
-      .map(() => Math.random() * 0.5); // Slower initial speed values
+      .map(() => Math.random() * 0.2); // Slower initial speed values
     startTimes.current = Array(columns).fill(Date.now());
     stopTimes.current = Array(columns)
       .fill(Date.now() + stopTime)
@@ -158,6 +191,26 @@ export const SlotMachine: React.FC = () => {
     }, 1000);
   };
 
+  const handleSpinRequest = async () => {
+    const result = await sendSpinRequest(betAmount);
+    console.log(result);
+    setWin(true);
+    setPayout(result.payout);
+    if (result.win){
+      setTimeout(() => {
+        setShowLine(true);
+        const lineIntervalId = setInterval(() => {
+          setShowLine((prevShowLine) => !prevShowLine);
+        }, 500);
+        setTimeout(() => {
+          clearInterval(lineIntervalId);
+          setShowLine(false);
+        }, totalSpinTime);
+      }, totalSpinTime + 1000);
+    }
+  };
+  
+
   const drawRect = (graphics: PIXI.Graphics) => {
     graphics.clear();
     graphics.lineStyle(5, 0x966304, 1);
@@ -167,12 +220,24 @@ export const SlotMachine: React.FC = () => {
       0,
       0,
       windowWidth * 0.11 * (windowWidth <= 768 ? 2 : 1),
-      slotHeight * rows,
+      slotHeight * rows * (windowWidth <= 768 ? 2 : 1),
       5
     );
     graphics.endFill();
   };
 
+  const drawLine = (graphics: PIXI.Graphics) => {
+    if (!showLine) {
+      graphics.clear();
+      return;
+    }
+    graphics.clear();
+    graphics.lineStyle(4, 0xff0000, 1); // Red color line with thickness 4
+    graphics.moveTo(0, slotHeight * 1.5);
+    graphics.lineTo(windowWidth * 0.391 * (windowWidth <= 768 ? 2 : 1), slotHeight * 1.5);
+    graphics.endFill();
+  };
+  
   return (
     <div
       className={`w-full h-full flex flex-col justify-center items-center gap-5 bg-[url(${slotBackground})] text-white overflow-hidden`}
@@ -188,7 +253,7 @@ export const SlotMachine: React.FC = () => {
         <Stage
           options={{ backgroundAlpha: 0 }}
           width={windowWidth * 0.391 * (windowWidth <= 768 ? 2 : 1)}
-          height={totalHeight}
+          height={totalHeight * (windowWidth <= 768 ? 2 : 1)}
         >
           {assetsMatrix.map((column, colIndex) => (
             <Container
@@ -204,22 +269,24 @@ export const SlotMachine: React.FC = () => {
               {[...column, ...column].map((asset, rowIndex) => (
                 <Sprite
                   key={rowIndex}
-                  image={asset}
+                  image={asset.image}
                   y={
                     ((positions[colIndex] + rowIndex * slotHeight) %
-                      (totalHeight * 2))
+                      (totalHeight * 2)) *
+                    (windowWidth <= 768 ? 2 : 1)
                   }
                   x={
-                    windowWidth <= 768
-                      ? windowWidth * 0.010
-                      : windowWidth * 0.005
+                    windowWidth >= 768
+                      ? windowWidth * 0.005
+                      : windowWidth * 0.004
                   }
                   width={windowWidth * 0.1 * (windowWidth <= 768 ? 2 : 1)}
-                  height={slotHeight}
+                  height={windowWidth <= 768 ? slotHeight * 2 : slotHeight}
                 />
               ))}
             </Container>
           ))}
+          {showLine && <Graphics draw={(g: PIXI.Graphics) => drawLine(g)} />}
         </Stage>
       </div>
 
@@ -254,7 +321,10 @@ export const SlotMachine: React.FC = () => {
 
         <button
           className=" bg-stone-900/50 p-[2.5%] rounded-full border-2 border-slate-200"
-          onClick={startSpinning}
+          onClick={()=>{
+            handleSpinRequest();
+            startSpinning();
+          }}
           disabled={spinning}
         >
           <RefreshCcw
@@ -264,7 +334,7 @@ export const SlotMachine: React.FC = () => {
 
         <div className="uppercase">
           <p className="font-semibold">last win:</p>
-          <p>0</p>
+          <p>{payout}</p>
         </div>
       </div>
     </div>
