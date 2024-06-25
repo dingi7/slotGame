a4af846f16ec5a8df51a5a8876d6912201b11a8a
Mityo Draganov <mityodraganow@gmail.com>
2024-06-25 12:35:56 +0300

    font changes

--- client/src/components/SlotMachine.tsx
+++ client/src/components/SlotMachine.tsx
@@ -19,13 +19,15 @@ export const SlotMachine: React.FC = () => {
   const fixedBetAmounts = [20, 40, 100, 200];
   const [betAmount, setBetAmount] = useState<number>(fixedBetAmounts[0]);
 
-  
   const columns = 3;
   const rows = 3;
 
   const [assetsMatrix, setAssetsMatrix] = useState<string[][]>(
     Array.from({ length: columns }, () =>
-      Array.from({ length: rows }, () => assets[Math.floor(Math.random() * assets.length)])
+      Array.from(
+        { length: rows },
+        () => assets[Math.floor(Math.random() * assets.length)]
+      )
     )
   );
   const [spinning, setSpinning] = useState(false);
@@ -52,7 +54,7 @@ export const SlotMachine: React.FC = () => {
           })
         );
 
-        if (spinSpeed.current.every(speed => speed === 0)) {
+        if (spinSpeed.current.every((speed) => speed === 0)) {
           setSpinning(false);
           setAssetsMatrix((prevMatrix) => realignMatrix(prevMatrix, positions));
         }
@@ -64,7 +66,9 @@ export const SlotMachine: React.FC = () => {
     currentTicker.add((ticker: PIXI.Ticker) => handleTick(ticker.deltaTime));
 
     return () => {
-      currentTicker.remove((ticker: PIXI.Ticker) => handleTick(ticker.deltaTime));
+      currentTicker.remove((ticker: PIXI.Ticker) =>
+        handleTick(ticker.deltaTime)
+      );
       currentTicker.stop();
     };
   }, [spinning]);
@@ -89,46 +93,23 @@ export const SlotMachine: React.FC = () => {
     );
     setAssetsMatrix(newMatrix);
 
-    spinSpeed.current = Array(columns).fill(0).map(() => Math.random() * 5 + 10);
+    spinSpeed.current = Array(columns)
+      .fill(0)
+      .map(() => Math.random() * 5 + 10);
     console.log(spinSpeed.current);
-    targetPositions.current = positions.map(() => (Math.floor(Math.random() * 20 + 20)) * slotHeight);
-    
+    targetPositions.current = positions.map(
+      () => Math.floor(Math.random() * 20 + 20) * slotHeight
+    );
+
     setPositions(Array(columns).fill(0));
   };
 
-
-  //   const handleBetAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //     const newValue = Number(e.target.value);
-
-  //     if (isNaN(newValue) || !isFinite(newValue)) {
-  //       return;
-  //     }
-  //     // Set the bet amount directly if the current state is 0
-  //     if (betAmount === 0) {
-  //       setBetAmount(newValue);
-  //     } else {
-  //       // You can implement additional logic here if needed
-  //       setBetAmount(newValue);
-  //     }
-  //   };
-
   const drawRect = (graphics: PIXI.Graphics) => {
-    const rectWidth =
-      windowWidth >= 768 ? windowWidth * 0.09 : windowWidth * 0.32;
-    const rectHeight =
-      windowWidth >= 768 ? windowWidth * 0.3 : windowWidth * 0.8;
     graphics.clear();
-    const padding = 0.1;
-    graphics.lineStyle(3, 0x966304, 1);
+    graphics.lineStyle(5, 0x966304, 1);
     const fillColor = 0x08043c;
     graphics.beginFill(fillColor, 1);
-    graphics.drawRoundedRect(
-      -padding,
-      -padding,
-      rectWidth + padding * 2,
-      rectHeight + padding * 2,
-      5
-    );
+    graphics.drawRoundedRect(0, 0, windowWidth * 0.11, slotHeight * rows, 5);
     graphics.endFill();
   };
 
@@ -145,42 +126,29 @@ export const SlotMachine: React.FC = () => {
 
       <div className="my-auto relative">
         <Stage
-          options={{
-            backgroundAlpha: 0,
-          }}
-          width={windowWidth >= 768 ? windowWidth * 0.31 : windowWidth * 0.9}
-          height={windowWidth >= 768 ? windowWidth * 0.3 : windowWidth * 0.8}
+          options={{ backgroundAlpha: 0 }}
+          width={windowWidth * 0.391}
+          height={totalHeight}
+
         >
-          {assetsMatrix.map((column, index) => (
-            <Container
-              x={
-                windowWidth >= 768
-                  ? index * windowWidth * 0.11
-                  : index * windowWidth * 0.32
-              }
-            >
+          {assetsMatrix.map((column, colIndex) => (
+            <Container x={(colIndex !== 0 ? colIndex : 0.01) * windowWidth * 0.14} key={colIndex}>
               <Graphics draw={(g: PIXI.Graphics) => drawRect(g)} />
-              {column.map((asset, index) => (
+              {[...column, ...column].map((asset, rowIndex) => (
                 <Sprite
-                  key={index}
+                  key={rowIndex}
                   image={asset}
                   y={
-                    windowWidth >= 768
-                      ? (index !== 0 ? index : 0.1) * windowWidth * 0.106
-                      : (index !== 0 ? index : 0.1) * windowWidth * 0.27
+                    (positions[colIndex] + rowIndex * slotHeight) %
+                    (totalHeight * 2)
                   }
                   x={
                     windowWidth >= 768
                       ? windowWidth * 0.005
                       : windowWidth * 0.004
                   }
-                  width={
-                    windowWidth >= 768 ? windowWidth * 0.08 : windowWidth * 0.26
-                  }
-                  height={
-                    windowWidth >= 768 ? windowWidth * 0.08 : windowWidth * 0.26
-                  }
-                  filters={[]}
+                  width={windowWidth * 0.1}
+                  height={slotHeight}
                 />
               ))}
             </Container>
@@ -190,28 +158,14 @@ export const SlotMachine: React.FC = () => {
 
       <div className="w-full md:w-[60%] mx-auto mb-0 mt-auto pb-4 flex  justify-evenly items-center select-none">
         <div>
-          <p className="uppercase">balance:</p>
+          <p className="uppercase font-semibold">balance:</p>
           <p>5000 BGN</p>
         </div>
-        {/* <div className="flex gap-4 text-2xl border-2 rounded-lg p-[0.2%]">
-          <button className="border-slate-200 border-2 py-[8%] px-4 rounded-tl-lg rounded-bl-lg shadow" onClick={() => setBetAmount(betAmount - betInterval)}>
-            -
-          </button>
-          <div className="flex flex-col justify-center">
-            <input
-              className="text-xl bg-transparent text-center appearance-none"
-              value={betAmount}
-              onChange={handleBetAmountChange}
-            />
-            <p className="text-lg uppercase text-center">total bet</p>
-          </div>
-          <button className="border-slate-200 border-2 py-[8%] px-4 rounded-tr-lg rounded-br-lg shadow" onClick={() => setBetAmount(betAmount + betInterval)}>
-            +
-          </button>
-        </div> */}
 
         <div className="flex flex-col gap-2">
-            <p className="uppercase text-xs text-slate-300">please, place your bet </p>
+          <p className="uppercase text-xs text-slate-300">
+            please, place your bet{" "}
+          </p>
           <div className="flex gap-4">
             {fixedBetAmounts.map((x) => (
               <div
@@ -231,23 +185,6 @@ export const SlotMachine: React.FC = () => {
           </div>
         </div>
 
-        {/* <div className="uppercase flex flex-col gap-4 border-2 px-[5%] rounded-b-[80%]">
-          <p className="font-bold text-xl">0</p>
-          <p>win</p>
-        </div> */}
-
-        {/* <button className="uppercase border-slate-200 border-2 p-2 rounded-lg">
-          <span className="w-[20px] h-[20px]"></span>
-          <p className="flex flex-col">
-            <span>max </span>
-            <span>bet</span>
-          </p>
-        </button> */}
-
-        {/* <button onClick={shuffleMatrix} className="uppercase">
-          <p>Spin</p>
-          <p>hold for auto</p>
-        </button> */}
         <button
           className=" bg-stone-900/50 p-[2.5%] rounded-full border-2 border-slate-200"
           onClick={shuffleMatrix}
@@ -256,7 +193,7 @@ export const SlotMachine: React.FC = () => {
         </button>
 
         <div className="uppercase">
-          <p>last win:</p>
+          <p className="font-semibold">last win:</p>
           <p>0</p>
         </div>
       </div>
