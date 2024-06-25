4cd96fd0d1d4d62f7f2b4fee931aa18101132009
Stefan Todorov <79049907+StefanTodorov32@users.noreply.github.com>
2024-06-25 17:02:45 +0300

    Update reel assets

--- client/src/assets/reelAssets.ts
+++ client/src/assets/reelAssets.ts
@@ -1,47 +1,52 @@
 export const assets = [
     {
       image:
-        "https://png.pngtree.com/png-clipart/20210123/ourmid/pngtree-black-colorful-number-1-png-image_2786493.jpg",
+        "https://static.vecteezy.com/system/resources/previews/008/880/294/original/number-0-gold-png.png",
+      value: 0,
+    },
+    {
+      image:
+        "https://static.vecteezy.com/system/resources/previews/011/993/493/original/number-1-gold-png.png",
       value: 1,
     },
     {
       image:
-        "https://png.pngtree.com/png-clipart/20210123/ourmid/pngtree-black-colorful-number-1-png-image_2786493.jpg",
+        "https://static.vecteezy.com/system/resources/previews/011/993/496/original/number-2-gold-png.png",
       value: 2,
     },
     {
       image:
-        "https://png.pngtree.com/png-clipart/20210123/ourmid/pngtree-black-colorful-number-1-png-image_2786493.jpg",
+        "https://static.vecteezy.com/system/resources/thumbnails/011/297/442/small/golden-number-3-png.png",
       value: 3,
     },
     {
       image:
-        "https://png.pngtree.com/png-clipart/20210123/ourmid/pngtree-black-colorful-number-1-png-image_2786493.jpg",
+        "https://static.vecteezy.com/system/resources/previews/013/453/439/original/gold-number-4-free-png.png",
       value: 4,
     },
     {
       image:
-        "https://png.pngtree.com/png-clipart/20210123/ourmid/pngtree-black-colorful-number-1-png-image_2786493.jpg",
+        "https://static.vecteezy.com/system/resources/previews/013/453/437/original/gold-number-5-free-png.png",
       value: 5,
     },
     {
       image:
-        "https://png.pngtree.com/png-clipart/20210123/ourmid/pngtree-black-colorful-number-1-png-image_2786493.jpg",
+        "https://static.vecteezy.com/system/resources/previews/008/880/295/original/number-6-gold-png.png",
       value: 6,
     },
     {
       image:
-        "https://png.pngtree.com/png-clipart/20210123/ourmid/pngtree-black-colorful-number-1-png-image_2786493.jpg",
+        "https://static.vecteezy.com/system/resources/previews/008/880/292/original/number-7-gold-png.png",
       value: 7,
     },
     {
       image:
-        "https://png.pngtree.com/png-clipart/20210123/ourmid/pngtree-black-colorful-number-1-png-image_2786493.jpg",
+        "https://static.vecteezy.com/system/resources/previews/008/880/293/original/number-8-gold-png.png",
       value: 8,
     },
     {
       image:
-        "https://png.pngtree.com/png-clipart/20210123/ourmid/pngtree-black-colorful-number-1-png-image_2786493.jpg",
+        "https://static.vecteezy.com/system/resources/previews/008/880/296/original/number-9-gold-png.png",
       value: 9,
     },
   ];
